017538a6ffe35d62d83d5f5270802e233f8bbbfd
paulaLopes2270 <plnhschltz@gmail.com>
2021-06-30 23:25:42 -0300

    arrays

--- início/arraysAndLoops/app.js
+++ início/arraysAndLoops/app.js
@@ -41,3 +41,34 @@ for (var i =0; i < videoGames3.length; i++) {
     break
   }
 }
+//FOREACH
+//é um método que executa uma função para cada item da Array. é uma forma mais simples de utilizarmos um loope com arrays (ou array-like)
+var videoGames4 = ['Switch', 'PS4', 'XBox', '3DS']; videoGames4.forEach(function(item, array, index){
+  console.log(array, index);//o atributo item será atribuido dinamicamente
+
+  //podemos passar os seguintes parâmetros item, index array
+});
+
+
+// Crie uma array com os anos que o Brasil ganhou a copa
+// 1959, 1962, 1970, 1994, 2002
+// Interaja com a array utilizando um loop, para mostrar
+// no console a seguinte mensagem, `O brasil ganhou a copa de ${ano}`
+basilCampeao = ['1959' , '1962', '1970', '1994', '2002'] 
+for (var i = 0; i < basilCampeao.length; i++) {
+  console.log(`O Brasil ganhou a copa de ${basilCampeao[i]}`)
+}
+
+// Interaja com um loop nas frutas abaixo e pare ao chegar em Pera
+var frutas = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+for(contador = 0; contador < frutas.length; contador++) {
+  console.log(frutas[contador])
+if(frutas[contador]=== 'Pera') {
+  break
+}
+}
+
+// Coloque a última fruta da array acima em uma variável,
+// sem remover a mesma da array.
+var frutas2 = ['Banana', 'Maçã', 'Pera', 'Uva', 'Melância']
+var ultimaFruta = frutas2[frutas2.length - 1];
